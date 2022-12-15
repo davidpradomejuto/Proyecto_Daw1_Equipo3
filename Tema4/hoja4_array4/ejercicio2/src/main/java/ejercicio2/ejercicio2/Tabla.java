92d845de75a3d6a42d3c04696c537775277740c2
Divux <davidprado228@gmail.com>
2022-12-15 13:55:20 +0100

    añado ejericico 2 hoja 4 array multidimensionales

---
+++ Tema4/hoja4_array4/ejercicio2/src/main/java/ejercicio2/ejercicio2/Tabla.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio2.ejercicio2;
+
+/**
+ *
+ * @author david
+ */
+public class Tabla {
+
+    private int[][] matriz = new int[4][5];
+
+    public Tabla() {
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 0; j < matriz[0].length; j++) {
+                int random = (int) Math.floor(Math.random() * 100) + 1;
+                matriz[i][j] = random;
+            }
+
+        }
+    }
+
+    public void mostrar() {
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + ",");
+            }
+            System.out.println("");
+        }
+    }
+
+    public int mayorF(int fila) {
+        int mayor = 0;
+        if (fila < matriz.length) {//si la fila existe
+
+            for (int i = 0; i < matriz[fila].length; i++) { //se repite en cada numero de esa fila
+                if (matriz[fila][i] >= mayor) {
+                    mayor = matriz[fila][i];
+                }
+            }
+            
+        } else {
+            System.out.println("error, la fila es incorrecta");//si la fila no existe manda este mesaje
+        }
+        return mayor;
+    }
+    
+    public int mayorC(int columna) {
+        int mayor = 0;
+        for (int i = 0; i < 1; i++) {
+            
+        }
+
+        
+        return mayor;
+    }
+
+}
