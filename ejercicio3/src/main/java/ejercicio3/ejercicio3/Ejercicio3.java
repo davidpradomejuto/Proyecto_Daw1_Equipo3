6fb761ce987a71448a3d4cc85f34c2e86b52ad43
Divux <davidprado228@gmail.com>
2022-12-18 20:13:51 +0100

    añado ejericico 3 hoja 5

---
+++ ejercicio3/src/main/java/ejercicio3/ejercicio3/Ejercicio3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicio3.ejercicio3;
+
+/**
+ *
+ * @author david
+ */
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+        Serie a = new Serie('*');
+        System.out.println(a.resultado());
+
+    }
+}
