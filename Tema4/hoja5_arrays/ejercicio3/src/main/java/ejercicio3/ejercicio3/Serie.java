c81d8e70ec573f4e442f5d253d0e604629ab4446
davidprado <david.pradomejuto@iesmiguelherrero.com>
2022-12-19 15:32:32 +0100

    actualizacion

--- Tema4/hoja5_arrays/ejercicio3/src/main/java/ejercicio3/ejercicio3/Serie.java
+++ Tema4/hoja5_arrays/ejercicio3/src/main/java/ejercicio3/ejercicio3/Serie.java
@@ -15,27 +15,33 @@ public class Serie {
     private double[] matriz;
     private char signo;
 
-    public Serie(char signo) {
+    public Serie(char letra) {
+        this.signo=letra;
         matriz = new double[4];
         for (int i = 0; i < matriz.length; i++) {
             Scanner Teclado = new Scanner(System.in);
             System.out.println("Introduce un numero: ");
             double numero = Teclado.nextDouble();
+            
+            matriz[i]= numero;
         }
     }
 
     public double resultado() {
         double resultado = 0;
-        if (signo ==43) {
+        
+        if (signo == ('+')) {
 
             for (int i = 0; i < matriz.length; i++) {
                 resultado += matriz[i];
+                
             }
-        } else if (signo == 42) {
+        } else if (signo == ('*')) {
 
             resultado = 1;
             for (int i = 0; i < matriz.length; i++) {
                 resultado *= matriz[i];
+                
             }
         }
         return resultado;
