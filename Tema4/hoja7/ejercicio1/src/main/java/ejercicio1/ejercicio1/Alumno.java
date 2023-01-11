51162129c0a60d469017a6d44c700141702818c9
davidprado <david.pradomejuto@iesmiguelherrero.com>
2023-01-11 17:08:54 +0100

    actualizacion

--- Tema4/hoja7/ejercicio1/src/main/java/ejercicio1/ejercicio1/Alumno.java
+++ Tema4/hoja7/ejercicio1/src/main/java/ejercicio1/ejercicio1/Alumno.java
@@ -14,49 +14,44 @@ import java.util.Scanner;
 public class Alumno {
 
     private String nombreA;
-    private int[] matriz;
+    private int[] notas;
 
     public Alumno(String nombre) {
         Scanner teclado = new Scanner(System.in);
 
         nombreA = nombre;
 
-        matriz = new int[5];
+        notas = new int[5];
         for (int i = 0; i < 5; i++) {
             System.out.println("Introduce la nota numero " + (i+1));
             int nota = teclado.nextInt();
-            matriz[i] = nota;
+            notas[i] = nota;
         }
 
     }
 
     public int mayor() {
-        Arrays.sort(matriz);
-        return matriz[matriz.length - 1];
+        Arrays.sort(notas);
+        return notas[notas.length - 1];
     }
 
     public int menor() {
-        Arrays.sort(matriz);
-        return matriz[0];
+        Arrays.sort(notas);
+        return notas[0];
     }
 
     public void ponerACero() {
-        Arrays.fill(matriz, 0);
+        Arrays.fill(notas, 0);
     }
 
     public int buscar(int newNota) {
-        Arrays.sort(matriz);
-        return Arrays.binarySearch(matriz, newNota);
+        Arrays.sort(notas);
+        return Arrays.binarySearch(notas, newNota);
     }
     
     public int[] tresMejores(){
-        int[] result = new int[3];
-        Arrays.sort(matriz);
-        
-        for (int i = matriz.length-1 ,x=0; i >= matriz.length-3; i--,x++) {
-            result[x] = matriz[i];
-        }
-        
-        return result;
+
+        Arrays.sort(notas); 
+        return Arrays.copyOfRange(notas, notas.length-3,notas.length);
     }
 }
