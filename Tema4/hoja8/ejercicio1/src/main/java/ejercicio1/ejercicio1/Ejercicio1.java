2f523653ebb2bf1c96196d26aceec9a094ce6fea
davidprado <david.pradomejuto@iesmiguelherrero.com>
2023-01-13 15:46:31 +0100

    actualizacion

--- Tema4/hoja8/ejercicio1/src/main/java/ejercicio1/ejercicio1/Ejercicio1.java
+++ Tema4/hoja8/ejercicio1/src/main/java/ejercicio1/ejercicio1/Ejercicio1.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package ejercicio1.ejercicio1;
 
 /**
@@ -12,16 +11,23 @@ package ejercicio1.ejercicio1;
 public class Ejercicio1 {
 
     public static void main(String[] args) {
-       int n1,n2,n3,total,result;
-       total =  Integer.valueOf(args[0]);
-       n1 = Integer.valueOf(args[0].substring(0, 1));
-       n2 = Integer.valueOf(args[0].substring(1, 2));
-       n3 = Integer.valueOf(args[0].substring(2, 3));
-       result = (int) (Math.pow(n1,3)+Math.pow(n2,3)+Math.pow(n3,3));
-        if(result == total){
-            System.out.println("Es un numero armstrong");
+        int total, result = 0;
+        total = Integer.valueOf(args[0]);
+        
+        if (args[0].length() == 3) {
+            
+            for (int i = 0; i <= 2; i++) {
+                result += (int) (Math.pow(Integer.valueOf(args[0].substring(i, i+1)), 3));
+            }
+            
+            
+            if (result == total) {
+                System.out.println("Es un numero armstrong");
+            } else {
+                System.out.println("No lo es");
+            }
         }else{
-            System.out.println("No lo es");
+            System.out.println("No es un numero de tres cifras");
         }
     }
 }
