5715a939033ef44bebf2fb497f0512cadab3d9b6
davidprado <david.pradomejuto@iesmiguelherrero.com>
2023-01-23 16:26:00 +0100

    actualizacion

---
+++ Tema5/hoja02/ejercicio3/src/main/java/ejercicio3/ejercicio3/Circulo.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio3.ejercicio3;
+
+/**
+ *
+ * @author DAW118
+ */
+public class Circulo extends Forma {
+
+    public Circulo() {
+        identificador = "Circulo";
+    }
+
+
+    @Override
+    public String toString() {
+        return "Identificador = " + identificador;
+    }
+    
+}
