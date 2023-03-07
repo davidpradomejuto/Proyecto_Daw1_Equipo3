47823689b49ca3767074785cbd4a1a8591293506
david <david@DESKTOP-3OPF9RG>
2023-03-07 18:44:29 +0100

    actu

---
+++ Tema5/teclado/src/main/java/ejercicio2/teclado/Teclado.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package ejercicio2.teclado;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author david
+ */
+public class Teclado {
+
+    public static void main(String[] args) {
+       pedirNombre();
+    }
+    
+    public static double pedirDouble() {
+        boolean valido = false;
+        double n = 0;
+
+        while (!valido) {
+            try {
+                System.out.println("Introduce el Numero: ");
+                n = new Scanner(System.in).nextDouble();
+                valido = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Error No es un numero");
+            } catch (Exception e) {
+                System.out.println("Error Indeterminado");
+            }
+        }
+
+        return n;
+    }
+
+    public static int pedirIntPositivo() {
+        boolean valido = false;
+        int n = 0;
+
+        while (!valido) {
+            try {
+                System.out.println("Introduce el Numero: ");
+                n = new Scanner(System.in).nextInt();
+                if (n > 0) {
+                    valido = true;
+                    
+                }else{
+                    System.out.println("Has introducido un numero negativo,repite");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error No es un numero");
+            } catch (Exception e) {
+                System.out.println("Error Indeterminado");
+            }
+        }
+
+        return n;
+    }
+
+    public static String pedirNombre() {
+        boolean nombrevalido = false;
+        String nombre = "";
+        while (!nombrevalido) {
+            try {
+                System.out.println("Introduce el nombre del alumno: ");
+                nombre = new Scanner(System.in).nextLine();
+                if (nombre.matches("[a-zA-Z]+.*\\s.*")) {
+
+                } else {
+                    throw new Exception("Error el nombre contiene algun caracter no valido");
+                }
+                nombrevalido = true;
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+
+        return nombre;
+    }
+}
