88061c1764a411d79508e03028ccde5b16621a94
david <david@DESKTOP-3OPF9RG>
2023-03-17 11:23:07 +0100

    actu

--- Tema6/Hoja03/Ejercicio1/src/main/java/tema5/ejercicio1/Academia.java
+++ Tema6/Hoja03/Ejercicio1/src/main/java/tema5/ejercicio1/Academia.java
@@ -17,37 +17,99 @@ public class Academia {
 
     public Academia() {
         lista = new LinkedList<>();
-
     }
 
-    public void añadirCursoFinal(Curso c) {
-        lista.addLast(c);
-        System.out.println("Curso añadido al final de la lista");
-    }
+    public void aniadirCursoFinal() {
+        int tamanio = lista.size();
+        try {
+            lista.addLast(new Curso());
 
-    public void insertarCurso(Curso c, int pos) {
-        lista.add(pos, c);
-        System.out.println("Curso añadido correctamente");
-    }
-
-    public void borrarCurso(int cod) {
-        for (Curso c : lista) {
-            if (c.getCodigo() == cod) {
-                lista.remove();
-                System.out.println("Curso borrado correctamente");
+            if (tamanio < lista.size()) {
+                System.out.println("El curso se ha añadido correctamente");
+            } else {
+                System.out.println("El curso no se ha añadido...");
             }
+        } catch (Exception e) {
+            System.out.println("Error indeterminado..");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void insertarCurso() {
+
+        int tamanio = lista.size();
+
+        try {
+
+            int pos = Teclado.pedirIntPositivo("Introduce la posicion en la que quieras meter el curso;");
+            Curso c = new Curso();
+
+            lista.add(pos, c);
+
+            if (tamanio < lista.size()) {
+
+                System.out.println("El curso se ha añadido correctamente");
+            } else {
+                System.out.println("El curso no se ha añadido...");
+            }
+        } catch (Exception e) {
+            System.out.println("Error indeterminado..");
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public void borrarCurso() {
+        int cod = Teclado.pedirIntPositivo("Introduce le codigo del curso que quieres borrar");
+        boolean encontrado = false;
+        try {
+            for (Curso c : lista) {
+                if (c.getCodigo() == cod && !encontrado) {
+                    encontrado = true;
+
+                    if (lista.remove(c)) {
+                        System.out.println("Curso borrado correctamente");
+                    } else {
+                        System.out.println("Error al borrar el curso");
+                    }
+                }
+            }
+            
+            if (!encontrado){
+                System.out.println("El curso no ha sido encontrado...");
+            }
+        } catch (Exception e) {
+            System.out.println("Error indeterminado..");
+            System.out.println(e.getMessage());
         }
     }
 
-    public void modificarnHoras(int cod, int nHoras) {
+    public void modificarnHoras() {
+        boolean encontrado = false;
+        int cod = Teclado.pedirIntPositivo("Introduce el codigo del curso: ");
+
         for (Curso c : lista) {
-            if (c.getCodigo() == cod) {
-                c.setnHoras(nHoras);
+            if (c.getCodigo() == cod && !encontrado) {
+                int nHoras = Teclado.pedirIntPositivo("Introduce el numero de horas a modificar: ");
+                encontrado = true;
+                try {
+                    c.setnHoras(nHoras);
+                    System.out.println("Horas modificadas");
+                } catch (Exception e) {
+                    System.out.println("Error indeterminado");
+                    System.out.println(e.getMessage());
+                }
             }
+        }
+
+        if (!encontrado) {
+            System.out.println("El curso no ha sido encontrado");
         }
     }
 
     public void mostrar() {
+        System.out.println("Mostrando datos de los cursos");
         for (Curso c : lista) {
             System.out.println(c.toString());
         }
--- Tema6/Hoja03/Ejercicio1/src/main/java/tema5/ejercicio1/Ejercicio1.java
+++ Tema6/Hoja03/Ejercicio1/src/main/java/tema5/ejercicio1/Ejercicio1.java
@@ -15,41 +15,32 @@ public class Ejercicio1 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         Academia a = new Academia();
-        Curso c1= new Curso();
-        Curso c2=new Curso(12,"2C",200);
-        Curso c3= new Curso();
-        Curso c4=new Curso(40,"3D",350);
+
         int opc;
-        System.out.println("Menú de la Academia");
-        System.out.println("");
-        System.out.println("1- Añadir un nuevo curso al final de la lista");
-        System.out.println("2- Insertar un nuevo curso en cualquier posición");
-        System.out.println("3- Borrar un curso dado su código");
-        System.out.println("4- Modificar el número de Horas de un curso dado su código");
-        System.out.println("5- Mostrar todos los cursos");
-        System.out.println("0- Salir");
+
         do {
-            System.out.println("Ingresa una opción: ");
+            System.out.println("Menu de la Academia");
+            System.out.print("1- Añadir un nuevo curso al final de la lista\n2- Insertar un nuevo curso en cualquier posicion\n3- Borrar un curso dado su código\n4- Modificar el numero de Horas de un curso dado su codigo\n");
+            System.out.print("5- Mostrar todos los cursos\n\"0- Salir\n");
+            
+            System.out.println("Ingresa una opcion: ");
             opc = teclado.nextInt();
             switch (opc) {
                 case 1 ->
-                    a.añadirCursoFinal(c1);
-                case 2 ->{
-                    System.out.println("Insertando datos...");
-                    a.insertarCurso(c2, 0);
-                    a.insertarCurso(c3, 1);
-                    a.insertarCurso(c4, 2);
+                    a.aniadirCursoFinal();
+                case 2 -> {
+                    a.insertarCurso();
                 }
                 case 3 ->
-                    a.borrarCurso(34);
+                    a.borrarCurso();
                 case 4 ->
-                    a.modificarnHoras(15, 350);
+                    a.modificarnHoras();
                 case 5 ->
                     a.mostrar();
                 case 0 ->
                     System.out.println("Saliendo...");
                 default ->
-                    System.out.println("Opción incorrecta");
+                    System.out.println("Opcion incorrecta");
             }
         } while (opc != 0);
     }
