98e772f43b2b2527794323869dc12a2c79b147ad
david <david@DESKTOP-3OPF9RG>
2023-03-21 15:41:18 +0100

    actu

---
+++ Tema6/hoja04/ejercicio4/src/main/java/hoja4/ejercicio4/Atleta.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hoja4.ejercicio4;
+
+/**
+ *
+ * @author david
+ */
+public class Atleta {
+    private int dorsal;
+    private String nombre;
+    private double tiempo;
+}
