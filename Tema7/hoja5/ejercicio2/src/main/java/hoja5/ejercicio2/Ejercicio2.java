3ad71e2221265bc8a0ea262e8c5f3e63931dfacb
david <david@DESKTOP-3OPF9RG>
2023-04-27 13:56:23 +0200

    unidad7

--- Tema7/hoja5/ejercicio2/src/main/java/hoja5/ejercicio2/Ejercicio2.java
+++ Tema7/hoja5/ejercicio2/src/main/java/hoja5/ejercicio2/Ejercicio2.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
-
 package hoja5.ejercicio2;
 
 import java.io.BufferedReader;
@@ -20,11 +19,11 @@ import java.util.Scanner;
 public class Ejercicio2 {
 
     public static void main(String[] args) {
-        File fichero = new File("D:\\Usuarios\\daw118\\Documents\\pruebas\\hoja5ej2.txt");
-        File ficherocopia = new File("D:\\Usuarios\\daw118\\Documents\\pruebas\\hoja5ej2segundaparte.txt");
+        File fichero = new File("C:\\Users\\david\\Desktop\\pruebasficheros\\hoja5ej2.txt");
+        File ficherocopia = new File("C:\\Users\\david\\Desktop\\pruebasficheros\\hoja5ej2segundaparte.txt");
         System.out.println("creando el fichero linea a linea");
         escribirLineaALinea(fichero);
-        leerLineaALinea(fichero,ficherocopia);
+        leerLineaALinea(fichero, ficherocopia);
     }
 
     public static void escribirLineaALinea(File fichero) {
@@ -50,22 +49,23 @@ public class Ejercicio2 {
             }
         }
     }
-    
+
     // el final de la lectura lo detecta con -1
-    public static void leerLineaALinea(File fichero,File ficherocopia) {
+    public static void leerLineaALinea(File fichero, File ficherocopia) {
         BufferedReader br = null;
+        BufferedWriter brw = null;
         String linea;
+
+        String caracter = Teclado.pedirVocal("Introduce una vocal...");
         try {
+            brw = new BufferedWriter(new FileWriter(ficherocopia));
             br = new BufferedReader(new FileReader(fichero));
-            
-            
-            String caracter = Teclado.pedirVocal("Introduce una vocal...");
-            
             //mientras el string que viene por el buffer no sea null se sigue
             while ((linea = br.readLine()) != null) {
                 String[] vector = linea.split("");
-                linea.replaceAll("[aeiou]*", caracter);
-                escribirFraseSinVocales(fichero,linea);
+                linea = linea.replaceAll("[aeiou]*", caracter);
+                brw.write(linea);
+                brw.newLine();
             }
         } catch (IOException ex) {
             System.err.println(ex.toString());
@@ -77,28 +77,14 @@ public class Ejercicio2 {
                     System.out.println("Error al cerrar");
                 }
             }
-        }
-    }
-    
-    public static void escribirFraseSinVocales(File fichero,String frase) {
-
-        BufferedWriter br = null;
-        
-        try {
-
-            br = new BufferedWriter(new FileWriter(fichero, true));
-            br.write(frase);
-            br.write(System.lineSeparator());
-        } catch (IOException ex) {
-            System.err.println(ex.toString());
-        } finally {
-            if (br != null) {
+            if (brw != null) {
                 try {
-                    br.close();
+                    brw.close();
                 } catch (IOException ex) {
                     System.out.println("Error al cerrar");
                 }
             }
         }
     }
+
 }
