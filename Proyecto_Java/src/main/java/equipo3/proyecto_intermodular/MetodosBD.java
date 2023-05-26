f4701e29d73da8afa31a2d7e2879f89e6a1a76b7
DAW107 <DAW107@IF03-25.iesmhp.local>
2023-05-26 16:00:42 +0200

    Inicializar base de datos

--- Proyecto_Java/src/main/java/equipo3/proyecto_intermodular/MetodosBD.java
+++ Proyecto_Java/src/main/java/equipo3/proyecto_intermodular/MetodosBD.java
@@ -4,6 +4,7 @@
  */
 package equipo3.proyecto_intermodular;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -14,6 +15,10 @@ import java.sql.SQLException;
  */
 public class MetodosBD {
 
+    private Connection getConnection() {
+        return AccesoBaseDatos.getInstance().getConn();
+    }
+
     public Cliente seleccionCliente(String UUID) {
         Cliente p = null;
         String sql = "Select UUID,dni,nombre,apellidos,telefono,direccion,localidad,fechaNacimiento from clientes where UUID=?";
@@ -21,7 +26,7 @@ public class MetodosBD {
             sentencia.setString(1, UUID);
             try ( ResultSet rs = sentencia.executeQuery();) {
                 if (rs.next()) {
-                    p = new Cliente(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getString(6), rs.getString(7), rs.getDate(8),null,null);
+                    p = new Cliente(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5), rs.getString(6), rs.getString(7), rs.getDate(8), null, null);
                     System.out.println(p.toString());
                 }
             }
@@ -50,4 +55,71 @@ public class MetodosBD {
         return cb;
     }
 
+    public boolean insertarCliente(Cliente cliente) {
+        boolean result = false;
+        String sql = "INSERT INTO pacientes(dni,nombre,telefono) VALUES (?,?,?)";
+        if (pedirClientePorUuidd(cliente.getDni()) == null) {
+
+            try ( PreparedStatement stmt = getConnection().prepareStatement(sql);) {
+
+                stmt.setString(1, cliente.getDni());
+                stmt.setString(2, cliente.getNombre());
+                stmt.setString(3, cliente.getTelefono());
+                stmt.setString(3, cliente.getTelefono());
+                stmt.setString(3, cliente.getTelefono());
+                stmt.setString(3, cliente.getTelefono());
+
+                int salida = stmt.executeUpdate();
+                if (salida != 1) {
+                    throw new Exception(" No se ha insertado/modificado un solo registro");
+                } else {
+                    result = true;
+                }
+
+            } catch (SQLException ex) {
+                // errores
+                System.out.println("SQLException: " + ex.getMessage());
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+        } else {
+            System.out.println("El cliente ya existe, no se puede crear otro con el mismo UUIDD");
+        }
+
+        return result;
+    }
+
+    public Cliente pedirClientePorUuidd(String uuidd) {
+
+        Cliente result = null;
+
+        //esta es la sentencia del prepare
+        String sql = "SELECT dni,nombre,telefono FROM pacientes WHERE dni=?";
+
+        try ( PreparedStatement stmt = getConnection().prepareStatement(sql);) {
+
+            //en el prepare sustituyo el UUIDD
+            stmt.setString(1, uuidd);
+
+            try ( ResultSet rs = stmt.executeQuery();) {
+                if (rs.next()) {
+                    //genero el objeto cliente si existe
+                    result = crearCliente(rs);
+
+                } else {
+                    System.out.println("Cliente no encontrado...");
+                }
+            }
+
+        } catch (SQLException ex) {
+            // errores
+            System.out.println("SQLException: " + ex.getMessage());
+        }
+        return result;
+    }
+    private Cliente crearCliente(final ResultSet rs) throws SQLException {
+        return new Cliente(rs.getString("dni"),
+                rs.getString("nombre"),
+                rs.getString("telefono"));
+    }
 }
